ab698c81b33035623e91b9e862d3aea3aa7f6ee8
muftee <mhmuftee@gmail.com>
2021-12-05 14:07:47 +0100

    :bug: allow access

--- src/main/java/io/mhmuftee/portfolio/PortfolioController.java
+++ src/main/java/io/mhmuftee/portfolio/PortfolioController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PortfolioController {
-    @CrossOrigin(origins = "http://192.168.2.124:9000/")
     @GetMapping(value = "/")
     String root(){
         return "To be implemented";
