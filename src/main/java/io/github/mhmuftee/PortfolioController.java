f6eebefdee125b3246eb07e17e2b36a661a49c62
muftee <mhmuftee@gmail.com>
2022-03-24 19:59:01 +0100

    :sparkles: add person api

--- src/main/java/io/github/mhmuftee/PortfolioController.java
+++ src/main/java/io/github/mhmuftee/PortfolioController.java
@@ -1,7 +1,6 @@
 package io.github.mhmuftee;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
